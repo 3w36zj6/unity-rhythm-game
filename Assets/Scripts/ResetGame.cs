bff91302d6d540e2e6ea18fef2aa18404f808021
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-01-18 01:16:38 +0900

    リセット実装

---
+++ Assets/Scripts/ResetGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ResetGame : MonoBehaviour {
+    // Start is called before the first frame update
+    public void ButtonClicked() {
+        SceneManager.LoadScene("GameScene");
+        Debug.Log("aaaa");
+    }
+}
